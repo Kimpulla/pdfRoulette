ad0861bc4f1ebfdca262c492378db22656dbbaef
Kimpulla <haukka.kimi@gmail.com>
2024-02-14 20:07:52 +0200

    Added error message.

--- app/index.tsx
+++ app/index.tsx
@@ -2,25 +2,49 @@ import { Link, router } from "expo-router";
 import React,  { useState } from 'react';
 import { StyleSheet, Text, View, Pressable } from 'react-native';
 import * as DocumentPicker from "expo-document-picker";
-import PdfRead from "./view";
+import { showMessage, hideMessage } from "react-native-flash-message";
+import FlashMessage from "react-native-flash-message"; 
 
 export default function HomeScreen() {
 
   const [uri, setUri] = useState('');
 
+  /**
+   * Creates error message, using react native flash message.
+   */
+  const message = () => {
+    showMessage({
+      message: "Wrong file format!",
+      type: "danger",
+    });
+  }
+
+  /**
+   * Function to select the document with a help of expo documentpicker.
+   * 
+   */
   const pickDocument = async () => {
 
     let result = await DocumentPicker.getDocumentAsync({});
 
     if (result.assets && result.assets.length > 0) {
       const uri = result.assets[0].uri;
+      if (uri.endsWith('.pdf')) {
       setUri(uri);
       sendData(uri); // Pass uri to sendData function
     } else {
-        console.log("No assets found in the result.");
+        message();
     }
+  } else {
+    message();
   }
+}
  
+  /**
+   * Function redirects user to "view" screen and sends uri data along with it.
+   * 
+   * @param uri pdf files path.
+   */
   const sendData = (uri:string) => { // Accept uri as an argument
     router.push({
       pathname: "/view",
@@ -32,6 +56,7 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
+      <FlashMessage position="top" />
       <View style={styles.background} />
       <View style={styles.contentContainer}>
         <Text style={styles.title}>Start shuffling</Text>
