4c20765fb25305039fef2e5c57e5027b9f7d6e3f
Kimpulla <haukka.kimi@gmail.com>
2024-02-17 02:41:29 +0200

    Created global colors file.

---
+++ app/colors.tsx
@@ -0,0 +1,17 @@
+const light = {
+    backgroundColor: 'white',
+    statusBarBackground: '#f5f5f5',
+    statusBarText: 'black',
+    text: 'black',
+    
+  };
+  
+  const dark = {
+    backgroundColor: '#121212',
+    statusBarBackground: '#262626',
+    statusBarText: 'white',
+    text: 'white',
+    
+  };
+
+  export default { light, dark };
